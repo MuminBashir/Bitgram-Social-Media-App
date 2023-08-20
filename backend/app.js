4bf198e05c01491fbf88747ef4898c591550c174
Mumin Bashir <moominbashir732@gmail.com>
2023-08-21 01:10:54 +0530

    made routes for register, login and post

--- backend/app.js
+++ backend/app.js
@@ -5,4 +5,16 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+// Using Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Importing routes
+const post = require("./routes/post");
+const user = require("./routes/user");
+
+// using Routes
+app.use("/api/v1", post);
+app.use("/api/v1", user);
+
 module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,65 @@
+const User = require("../models/User");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    user = await User.create({
+      name,
+      email,
+      password,
+      avatar: { public_id: "sample", url: "sample" },
+    });
+
+    res.status(201).json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const token = await user.generateToken();
+
+    res.status(200).cookie("token", token).json({
+      success: true,
+      user,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
