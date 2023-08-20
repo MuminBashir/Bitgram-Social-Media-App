2442907f93aaf1aa166068f5b79b775691249f81
Mumin Bashir <moominbashir732@gmail.com>
2023-08-20 17:46:13 +0530

    made post and user schema

---
+++ backend/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: [true, "Email already exists"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [8, "Password must be atleast 8 characters"],
+    select: false,
+  },
+
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
