becd1c3f250672324d93f28834ef8e984cc4098c
Mumin Bashir <moominbashir732@gmail.com>
2023-08-21 19:39:07 +0530

    added getuser and getallusers and getmyprofile to user route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -272,3 +272,59 @@ exports.deleteUser = async (req, res) => {
     });
   }
 };
+
+exports.myProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).populate("posts");
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getUserProfile = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.params.id).populate("posts");
+
+    if(!user){
+      return   res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+    
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.status(200).json({
+      success: true,
+      users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,6 +7,9 @@ const {
   updatePassword,
   updateProfile,
   deleteUser,
+  myProfile,
+  getUserProfile,
+  getAllUsers,
 } = require("../controllers/user");
 const { isAuthenticated } = require("../middlewares/auth");
 
@@ -19,5 +22,8 @@ router.route("/logout").get(isAuthenticated, logout);
 router.route("/update/password").put(isAuthenticated, updatePassword);
 router.route("/update/profile").put(isAuthenticated, updateProfile);
 router.route("/delete/me").delete(isAuthenticated, deleteUser);
+router.route("/me").get(isAuthenticated, myProfile);
+router.route("/user/:id").get(isAuthenticated, getUserProfile);
+router.route("/users").get(isAuthenticated, getAllUsers);
 
 module.exports = router;
