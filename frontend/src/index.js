27523ed4f3a47c496b19171606680bef87ee6871
Mumin Bashir <moominbashir732@gmail.com>
2023-08-23 19:51:01 +0530

    deleted frontend folder

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import reactDOM from "react-dom/client";
-
-import App from "./App";
-
-const root = reactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
