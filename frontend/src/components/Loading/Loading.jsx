5128ab972cfe7be1dfbb203df001e7f488475669
Mumin Bashir <moominbashir732@gmail.com>
2023-09-07 00:01:40 +0530

    added loading component

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Loading.css";
+import CircularProgress from '@mui/material/CircularProgress';
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="loadingPage">
+       <CircularProgress color="primary" />
+    </div>
+  );
 };
 
 export default Loading;
