0a39fe899645eeaa6281ea3000fd7a3db1928cf4
Mumin Bashir <moominbashir732@gmail.com>
2023-09-07 21:49:19 +0530

    fixed vercel hosting address

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== "production") {
 //Setting cors for frontend
 app.use(
   cors({
-    origin: ["https://bitgram.vercel.app/register"],
+    origin: ["https://bitgram.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
