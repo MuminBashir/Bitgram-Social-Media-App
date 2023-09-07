a6eda047196dfa14aee9288fd67efddb1ea06582
Mumin Bashir <moominbashir732@gmail.com>
2023-09-07 23:22:45 +0530

    allow backend to set cookie

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -28,6 +28,9 @@ exports.register = async (req, res) => {
     const token = await user.generateToken();
 
     const options = {
+      domain: "https://bitgram.vercel.app",
+      path: "/",
+      secure: true,
       expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
       httpOnly: true,
     };
